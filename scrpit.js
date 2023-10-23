862f2363e49c9adb41595b7b99f1a7c90b2da565
RenanVictorRsw <renanvictor12rsw@outlook.com>
2023-10-23 20:20:52 -0300

    comitei

---
+++ scrpit.js
@@ -0,0 +1,28 @@
+
+let loginBtn = document.querySelector("#loginbtn")
+let LoginMenu = document.querySelector("#login")
+let x = document.querySelector("#fechar")
+let formulario = document.querySelector("#formulario")
+
+
+LoginMenu.addEventListener("click", () => {
+
+    formulario.style.display = "flex"
+
+
+
+
+})
+
+
+
+x.addEventListener("click", () => {
+
+formulario.style.display = "none"
+
+})
+
+
+
+
+
